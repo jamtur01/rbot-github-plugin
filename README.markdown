72e230ff14499217992471f86a5ebd3bbf8c545c
James Turnbull <james@lovedthanlost.net>
2011-01-11 18:50:10 +1100

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,34 @@
+GitHUb Plugin for Rbot
+======================
+
+Copyright 2011 James Turnbull <james@lovedthanlost.net>
+
+Licensed: GPLv3
+
+Requires
+--------
+
+Requires the following:
+
+* octopi
+
+Install
+-------
+
+1.  Copy into the Rbot plugins directory
+2.  Add it to list of loaded Rbot plugins
+3.  Restart Rbot
+
+Usage
+-----
+
+1.  Configure a channel to be monitored:
+
+    github.repomap:
+    - "#channelname:github_user:github_repo"
+
+2.  Ask the bot to provide you with information from GitHub
+
+  * `botname: #11`
+  * `botname: commit:SHA`
+
---
+++ github.rb
@@ -0,0 +1,71 @@
+require 'rubygems'
+require 'octopi'
+
+class GitHubPlugin < Plugin
+
+        include Octopi
+
+        Config.register Config::ArrayValue.new('github.repomap',
+                :default => [], :requires_restart => false,
+                :desc => "A map of channels to GitHub repos. Format '#channel:github_user:github_repo' " +
+                         "For example, '#vagrant:mitchellh:vagrant' " +
+                         "Currently only one project can be monitored per channel.")
+
+	def help(plugin, topic = "")
+		case topic
+			when '':
+				"github: Queries GitHub information. " +
+                                "Ask the bot for a commit or an issue using: " +
+                                "bot: #11 - where #11 is the issue number. " +
+                                "bot: commit:SHA - where SHA is the commit SHA."
+		end
+	end
+
+	def listen(m)
+		# Ignore things unless we're directly talked to
+		return unless m.address?
+
+                # Return unless we're addressed properly
+		return unless m.kind_of?(PrivMessage) && m.public?
+
+                refs = m.message.scan(/(?:^|\W)(\[\S+\]|\#\d+|commit:\w+)(?:$|\W)/).flatten
+
+                return unless refs.length > 0
+
+                refs.each do |ref|
+
+                  answer = github_query(ref, m.target)
+
+                  m.reply "#{m.sourcenick} #{ref} is #{answer}"
+                end
+        end
+
+	private
+        def github_query(ref, target)
+
+          u, r = repo_channel(target)
+
+          case
+          when ref.match(/^\#(\d+)/)
+            user = Octopi::User.find(u)
+            repo = user.repository(r)
+            issue = repo.issue($1)
+            answer = "'#{issue.title}' which was created #{issue.created_at} and has a status of #{issue.state.capitalize}"
+          when ref.match(/^commit\:(\w+)/)
+            commit = Octopi::Commit.find(:user => u, :repo => r, :sha => $1)
+            answer = "'#{commit.message}' and was created by #{commit.author['name']}"
+          end
+
+          return answer
+        end
+
+        def repo_channel(target)
+               @bot.config['github.repomap'].each { |l|
+                     l.scan(/^#{target}\:(.+)\:(.+)/) { |w|
+                       return $1, $2
+                     }
+               }
+        end
+end
+
+plugin = GitHubPlugin.new
